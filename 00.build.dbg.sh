08ac814699bf569c95fa32e08149c1af06ffde43
gxflying <gxflying@aliyun.com>
2025-08-19 10:15:03 +0800

    add cortex-m55-remote sim cmd

--- 00.build.dbg.sh
+++ 00.build.dbg.sh
@@ -6,8 +6,8 @@
 #sudo scripts/install_dependencies.sh
 
 # 3. 设置构建目录
-export BUILD_DIR="${PWD}/build_ubuntu_dbg"
-export INSTALL_DIR="${PWD}/install_ubuntu_dbg"
+export BUILD_DIR="${PWD}/build_dbg"
+export INSTALL_DIR="${PWD}/install_dbg"
 export ARCH=aarch64
 
 mkdir -p $BUILD_DIR $INSTALL_DIR
@@ -15,9 +15,9 @@ mkdir -p $BUILD_DIR $INSTALL_DIR
 # 4. 配置（以 Release + gcc/g++ 为例）
 cd $BUILD_DIR
 
-#  cmake  ..  -DCMAKE_BUILD_TYPE=Debug   -DCMAKE_C_COMPILER=gcc   -DCMAKE_CXX_COMPILER=g++   -DGREENSOCS_GIT="https://github.com/quic/"   -DLIBQEMU_TARGETS="aarch64;hexagon"   -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR
+ cmake  ..  -DCMAKE_BUILD_TYPE=Debug   -DCMAKE_C_COMPILER=gcc   -DCMAKE_CXX_COMPILER=g++   -DGREENSOCS_GIT="https://github.com/quic/"   -DLIBQEMU_TARGETS="aarch64;hexagon"   -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR
 
-  cmake  ..  -DCMAKE_BUILD_TYPE=Debug   -DCMAKE_C_COMPILER=gcc   -DCMAKE_CXX_COMPILER=g++   -DGREENSOCS_GIT="https://github.com/quic/"   -DUBUNTU_ARCH=$ARCH -DLIBQEMU_TARGETS="$ARCH"   -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR
+  # cmake  ..  -DCMAKE_BUILD_TYPE=Debug   -DCMAKE_C_COMPILER=gcc   -DCMAKE_CXX_COMPILER=g++   -DGREENSOCS_GIT="https://github.com/quic/"   -DUBUNTU_ARCH=$ARCH -DLIBQEMU_TARGETS="$ARCH"   -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR
 
 # 5. 构建
 make install -j 8
---
+++ 02.run.sh
@@ -0,0 +1,1 @@
+./install_dbg/bin/platforms/cortex-m55-remote/cortex-m55-vp -l ./platforms/cortex-m55-remote/conf.lua 
